35829e86b610c66ffa34a1fce02e1860d77ae468
Maximilian Kromer <a.makro@web.de>
2020-02-15 19:05:09 +0100

    corrected last_position check

--- js/event.js
+++ js/event.js
@@ -80,7 +80,7 @@ function showEvent(event) {
     time.innerText = 'um ' + event.time.substr(0, 5) + ' Uhr';
     location_header.innerText = 'Ort:';
     loc_name.innerText = event.location;
-    if (last_position.coords != null) {
+    if (last_position != null && last_position != 'undefined') {
         distance.innerText = 'etwa ' + calcDistance(event.latitude, event.longitude) + 'km entfernt';
     }
     text.innerText = event.description;
@@ -90,7 +90,7 @@ function showEvent(event) {
     card.appendChild(time);
     card.appendChild(location_header);
     card.appendChild(loc_name);
-    if (last_position.coords != null) {
+    if (last_position != null && last_position != 'undefined') {
         card.appendChild(distance);
     }
     card.appendChild(text);
