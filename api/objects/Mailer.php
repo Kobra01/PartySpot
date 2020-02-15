b5725c68d31f8d45241d8939d71c4e72c6439cf7
Maximilian Kromer <a.makro@web.de>
2020-02-15 23:01:53 +0100

    added submit endpoint with phpmailer

---
+++ api/objects/Mailer.php
@@ -0,0 +1,76 @@
+<?php
+
+// 'Email' Object
+class Mailer {
+    
+    // database connection and table name
+    private $mailer;
+    private $table_name = "codes";
+    private $email_max = "a.makro@web.de";
+    private $email_finn = "a.makro@web.de";
+
+    // object properties
+    public $content;
+
+    // constructor
+    public function __construct($phpmail){
+        $this->mailer = $phpmail;
+    }
+
+    // send Mail Functions
+    public function sendSubmitMail() {
+
+        try {
+
+            //Recipients
+            $this->mailer->setFrom('noreply@mks-software.de', 'noreply@mks-software.de');
+            $this->mailer->addAddress($this->email_max);
+            // $this->mailer->addAddress($this->email_finn);
+
+            //Content
+            $this->mailer->isHTML(true);                                    // Set email format to HTML
+            $this->mailer->Subject = 'PS - Neuer Event Vorschlag';
+            $this->mailer->Body    = $this->content;
+            $this->mailer->AltBody = strip_tags($this->content);
+
+            $this->mailer->send();
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /*
+    public function sendPasswordResetMail() {
+
+        try {
+
+            //Recipients
+            $this->mailer->setFrom('noreply@mks-software.de', 'noreply@mks-software.de');
+            $this->mailer->addAddress($this->email);                        // Name is optional
+
+            //Content
+            $this->mailer->isHTML(true);                                    // Set email format to HTML
+            $this->mailer->Subject = 'SMS - Passwort zurücksetzen';
+            $etext = '<p>   Guten Tag, <br>
+                            Hier können sie ihr Passwort zurücksetzen. <br>
+                            Bitte klicken sie auf diesen <a href="https://mks-software.de/sms/resetpassword.html?code='.$this->code.'">Link</a> <br>
+                            <br>
+                            oder kopieren diese URL in ihren Browser: <br>
+                            https://mks-software.de/sms/resetpassword.html?code='.$this->code.' </p>';
+            $this->mailer->Body    = $etext;
+            $this->mailer->AltBody = strip_tags($etext);
+
+            $this->mailer->send();
+        } catch (Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+    */
+
+}
+
+?>
---
+++ api/submit_event.php
@@ -0,0 +1,54 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+// files needed to connect to database
+include_once 'objects/Mailer.php';
+
+// files and uses for sending email
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'libs/phpmailer/src/Exception.php';
+require 'libs/phpmailer/src/PHPMailer.php';
+require 'libs/phpmailer/src/SMTP.php';
+
+// instantiate mail object
+$phpmailer = new PHPMailer(true);                              // Passing `true` enables exceptions
+include_once 'config/Mailer.php';
+
+// instantiate other objects
+$mailer = new Mailer($phpmailer);
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+// check if data is set
+if (!isset($data->content)) {
+
+    // message if value missed
+    http_response_code(400);
+    echo json_encode(array("error" => TRUE, "message" => "No content."));
+    die();
+}
+
+// Prepare sending Email
+$mailer->content = $data->content;
+
+if (!$mailer->sendSubmitMail()) {
+
+    // message if unable to send email
+    http_response_code(400);
+    echo json_encode(array("error" => TRUE, "message" => "Unable to send email."));
+    die();
+}
+
+// set response code & answer
+http_response_code(201);
+echo json_encode(array("error" => FALSE, "message" => "Email was sent."));
+
+?>
